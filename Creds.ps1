1d38385330fd915da31aa2c180c1cbae4356a5ac
subjectdatadotcom <raja.bandla@subjectdata.com>
2025-04-08 10:36:01 -0500

    Update Creds.ps1

--- Creds.ps1
+++ Creds.ps1
@@ -1,38 +1,38 @@
 ﻿
 <#
 # Define source site credentials
-$srcUsername = "RBandla@flexera.com"
+$srcUsername = "RBandla@test.com"
 $srcPasswordText = ""
 
 
 $srcPassword = ConvertTo-SecureString $srcPasswordText -AsPlainText -Force
 
-$TargetConn = Connect-Site -Url  https://flexera.sharepoint.com/sites/Spot_EcoTest -Browser -Username $srcUsername -Password $srcPassword
+$TargetConn = Connect-Site -Url  https://test.sharepoint.com/sites/Spot_EcoTest -Browser -Username $srcUsername -Password $srcPassword
 
 
-Connect-Site -Url  https://flexera.sharepoint.com/sites/Spot_sharegate_test_teamsite -UseCredentialsFrom $TargetConn
+Connect-Site -Url  https://test.sharepoint.com/sites/Spot_sharegate_test_teamsite -UseCredentialsFrom $TargetConn
 ^i$b$h5U0E#H6;yT54+a8%P07l(hVShL
 #>
 
-NestorW@m365x76832558.onmicrosoft.com,RBandla@flexera.com
+NestorW@m365x76832558.onmicrosoft.com,RBandla@test.com
 
-https://m365x76832558.sharepoint.com/sites/benefits,https://flexera.sharepoint.com/sites/Spot_EcoTest
+https://m365x76832558.sharepoint.com/sites/benefits,https://test.sharepoint.com/sites/Spot_EcoTest
 
 
     <Mapping>
       <Source AccountName="allcompany@m365x76832558.onmicrosoft.com" DisplayName="allcompany@m365x76832558.onmicrosoft.com" PrincipalType="None" />
-      <Destination AccountName="Spot_Sharegate_Test_TeamSite@FLEXERA.onmicrosoft.com" DisplayName="Spot_Sharegate_Test_TeamSite@FLEXERA.onmicrosoft.com" PrincipalType="SecurityGroup" />
+      <Destination AccountName="Spot_Sharegate_Test_TeamSite@test.onmicrosoft.com" DisplayName="Spot_Sharegate_Test_TeamSite@test.onmicrosoft.com" PrincipalType="SecurityGroup" />
     </Mapping>
 
 
 
 
-$mappingsettings = Set-UserAndGroupMapping -Source "allcompany@m365x76832558.onmicrosoft.com" -Destination "Spot_Sharegate_Test_TeamSite@FLEXERA.onmicrosoft.com"
+$mappingsettings = Set-UserAndGroupMapping -Source "allcompany@m365x76832558.onmicrosoft.com" -Destination "Spot_Sharegate_Test_TeamSite@test.onmicrosoft.com"
 Export-UserAndGroupMapping -MappingSettings $mappingsettings -Path "C:\Users\RBandla\Documents\SG_MigrationScripts\cmdM365"
 
 
 $srcSite = Connect-Site -Url "https://m365x76832558.sharepoint.com/sites/allcompany" -UseCredentialsFrom $SourceConn
-$dstSite = Connect-Site -Url "https://flexera.sharepoint.com/sites/Spot_Sharegate_Test_TeamSite" -UseCredentialsFrom $TargetConn
+$dstSite = Connect-Site -Url "https://test.sharepoint.com/sites/Spot_Sharegate_Test_TeamSite" -UseCredentialsFrom $TargetConn
 
 $mp = Get-UserAndGroupMapping -SourceSite $srcSite -DestinationSite $dstSite
 $mp.UserAndGroupMappings | Format-List >> "C:\Users\RBandla\Documents\SG_MigrationScripts\mp.txt"
